d1630fb2113f394c7b638f7dbd816803ebfd15ea
Tim Mitra <tim@it-guy.com>
2023-12-05 22:13:41 -0500

    Initial Commit

---
+++ SwiftDataLearning/SwiftDataLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataLearningApp.swift
+//  SwiftDataLearning
+//
+//  Created by Tim Mitra on 2023-12-05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
