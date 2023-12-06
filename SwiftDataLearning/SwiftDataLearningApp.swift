886c22408f6a9e0e3845367237cb0ec50d4f01b8
Tim Mitra <tim@it-guy.com>
2023-12-05 22:17:38 -0500

    set up Swift Data model

--- SwiftDataLearning/SwiftDataLearningApp.swift
+++ SwiftDataLearning/SwiftDataLearningApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct SwiftDataLearningApp: App {
@@ -13,5 +14,6 @@ struct SwiftDataLearningApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Video.self)
     }
 }
