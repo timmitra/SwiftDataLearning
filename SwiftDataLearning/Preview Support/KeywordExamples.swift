8975377e7835697bd4f14d7f4d7bd3e0f787b678
Tim Mitra <tim@it-guy.com>
2023-12-08 16:46:40 -0500

    added keyword examples

---
+++ SwiftDataLearning/Preview Support/KeywordExamples.swift
@@ -0,0 +1,19 @@
+//
+//  KeywordExamples.swift
+//  SwiftDataLearning
+//
+//  Created by Tim Mitra on 2023-12-06.
+//
+
+import Foundation
+
+extension Keyword {
+  static var sampleKeywords: [Keyword] {
+    [
+      Keyword(title: "Sci Fi"),
+      Keyword(title: "Action"),
+      Keyword(title: "Drama"),
+      Keyword(title: "Comedy")
+    ]
+  }
+}
